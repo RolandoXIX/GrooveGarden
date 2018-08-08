020c42651b81e5f4679282e9eb65ab25929de0ad
Daniel Valverde <danielfvalverde@gmail.com>
2018-08-08 11:49:29 +0300

    asd

--- salas/urls.py
+++ salas/urls.py
@@ -7,10 +7,9 @@ urlpatterns = [
     url(r'^$', views.inicio, name='inicio'),
     url(r'^promociones/$', views.promociones, name='promociones'),
     url(r'^reserva/$', views.reserva, name='reserva'),
-    url(r'^turnos/$', views.selector_turnos),
-    url(r'^confirmacion/$', views.confirmacion),
-    url(r'^reserva_confirmada/$', views.reserva_confirmada),
-    url(r'^reserva/(\d{4})/(\d{2})/(\d{2})/$', views.selector_turnos),
-    url(r'^reserva/(\d{4})/(\d{2})/(\d{2})/confirmation/desde=(\d{2}:\d{2})tiempo=(\d{1}.\d{1})/$', views.confirmacion),
+    url(r'^reserva/turnos/$', views.turnos),
+    url(r'^reserva/confirmacion/$', views.confirmacion),
+    url(r'^reserva/confirmada/$', views.confirmada),
+
     ]
 
--- salas/views.py
+++ salas/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response, render
 import datetime
 from django.shortcuts import redirect
 from .models import Reserva
+from django.views.decorators.csrf import csrf_exempt
 
 def inicio(request):
     return render_to_response('inicio.html', context={"active":"inicio"})
@@ -10,16 +11,13 @@ def promociones(request):
     return render_to_response('promociones.html', context={"active":"promociones"})
 
 def reserva(request):
-    if request.GET:
-        date = datetime.datetime.strptime(request.GET.get('fecha'), "%Y-%m-%d").date().__format__('%Y/%m/%d')
-        return redirect(date)
-    else:
-        return render_to_response(
-            'form_reserva.html',
-        )
+    return render_to_response(
+        'form_reserva.html',
+    )
 
-def selector_turnos(request, year, month, day):
-    date = f'{day}/{month}/{year}'
+@csrf_exempt
+def turnos(request):
+    date = request.POST["fecha"]
     turnos = (
         ('12:00', '12:00'),
         ('12:30', '12:30'),
@@ -38,69 +36,58 @@ def selector_turnos(request, year, month, day):
         ('19:00', '19:00'),
         ('19:30', '19:30'),
     )
-    if request.GET:
-        horarios = dict(request.GET)
-        desde = datetime.timedelta(hours=12, minutes=00)
+
+    return render_to_response(
+        'selector_turnos.html',
+        context={
+            "active": "reserva",
+            "horarios": turnos,
+            "date": date,
+        }
+    )
+
+@csrf_exempt
+def confirmacion(request):
+    horarios = dict(request.POST)
+
+    date = horarios.pop("date")[0]
+
+    desde = datetime.timedelta(hours=12, minutes=00)
+    desde_str = str(desde)[0:5]
+    while desde_str not in horarios:
+        desde += datetime.timedelta(minutes=30)
         desde_str = str(desde)[0:5]
-        while desde_str not in horarios:
-            desde += datetime.timedelta(minutes=30)
-            desde_str = str(desde)[0:5]
 
-        tiempo = (len(horarios) - 1) / 2
-        hasta = desde + datetime.timedelta(hours=tiempo)
-        hasta_str = str(hasta)[0:5]
+    tiempo = (len(horarios) - 1) / 2
+    hasta = desde + datetime.timedelta(hours=tiempo)
+    hasta_str = str(hasta)[0:5]
 
-        url = 'confirmation/' + 'desde=' + desde_str + 'tiempo=' + str(tiempo)
-        return redirect(url)
+    return render_to_response(
+        'confirmacion.html',
+        context={
+            "active": "reserva_confirmada",
+            "date": date,
+            "tiempo": tiempo,
+            "desde_str": desde,
+            "hasta_str": hasta_str
+        }
+    )
 
-    else:
-        return render_to_response(
-            'selector_turnos.html',
-            context={
-                "active": "reserva",
-                "horarios": turnos,
-                "date": date,
-            }
-        )
+@csrf_exempt
+def confirmada(request):
+    date = request.POST.get('fecha')
+    time = request.POST.get('duracion')
+    minutes = int(float(time) * 60)
+    sala = request.POST.get('sala')
+    user = request.POST.get('user')
+    reserva = Reserva()
 
+    for d in range(0, minutes):
+        #reserva.sala = sala
+        reserva.usuario = user
+        reserva.dia = date
+        reserva.horario = time + datetime.timedelta(minutes=d)
+        d += 30
+        reserva.save()
 
-def confirmacion(request, year, month, day, desde, tiempo):
-    if request.method == "POST":
-        date = request.POST.get('fecha')
-        time = request.POST.get('duracion')
-        minutes = int(float(tiempo) * 60)
-        sala = request.POST.get('sala')
-        user = request.POST.get('user')
-        reserva = Reserva()
-
-        for d in range(0, minutes):
-            reserva.sala = sala
-            reserva.dia = date
-            reserva.usuario = user
-            reserva.horario = time + datetime.timedelta(minutes=d)
-            d += 30
-            reserva.save()
-
-        return render(request, 'reserva_confirmada.html', context={"active": "reserva_confirmada"})
-
-    else:
-        date = f'{day}/{month}/{year}'
-        time_hour = int(desde[:2])
-        time_minute = int(desde[3:])
-        time = datetime.timedelta(hours=time_hour, minutes=time_minute)
-        hasta = time + datetime.timedelta(hours=float(tiempo))
-        hasta_str = str(hasta)[:5]
-        return render_to_response(
-            'confirmacion.html',
-            context={
-                "active": "reserva_confirmada",
-                "date": date,
-                "tiempo": tiempo,
-                "desde_str": desde,
-                "hasta_str": hasta_str
-            }
-        )
-
-
-def reserva_confirmada(request):
     return render_to_response('reserva_confirmada.html', context={"active": "reserva_confirmada"})
